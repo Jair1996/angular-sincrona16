31bcea13a9c187fe2ad755a4d2dcee9825f091de
Jair Deyner <jairdeyner730791@gmail.com>
2023-01-11 09:36:53 -0500

    Routes config

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsModule } from '../components/components.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    LoginPageComponent
+  ],
+  exports: [
+    HomePageComponent,
+    LoginPageComponent
+  ],
   imports: [
     CommonModule,
     ComponentsModule
